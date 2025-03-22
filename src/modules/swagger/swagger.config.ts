128193e1254e87a1f0f4203726e57a9d8b380bb0
elyasbr <78977021+elyasbr@users.noreply.github.com>
2025-03-22 16:02:48 +0330

    1.0.3

---
+++ src/modules/swagger/swagger.config.ts
@@ -0,0 +1,35 @@
+import { DocumentBuilder, SwaggerCustomOptions } from '@nestjs/swagger';
+
+export class SwaggerConfig {
+  static createConfigDevelope(title:string ,version: string) {
+    return  new DocumentBuilder()
+      .setTitle(title)
+      .setVersion(version)
+      .addServer('http://localhost:3000/', 'Local environment' )
+      .addTag('Your API Tag')
+      .addBearerAuth(
+        {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+          name: 'JWT',
+          description: 'Enter JWT token',
+          in: 'header',
+        } ,
+      )
+      .addSecurityRequirements('bearer')
+      .build()
+
+  }
+
+  static createCustomOptions(): SwaggerCustomOptions {
+    return {
+      customSiteTitle: 'User Management API',  // تغییر عنوان صفحه
+      customfavIcon: 'https://example.com/favicon.ico',  // تغییر آیکون
+      customCss: `
+        .topbar { background-color: #333666; }
+        .swagger-ui .info { margin: 20px 0; }
+      `,  // اضافه کردن CSS سفارشی
+    };
+  }
+}
---
+++ src/modules/swagger/swagger.module.ts
@@ -0,0 +1,16 @@
+import { INestApplication, Module } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerConfig } from './swagger.config';
+
+@Module({})
+export class SwaggerModuleConfig {
+  static setup(app: INestApplication) :Array<string> {
+    let final:Array<string>=[]
+    const documentVersion1="docs/version1"
+    final = final.concat([documentVersion1 ])
+    const configV1 = SwaggerConfig.createConfigDevelope("CDN" ,'1.0');
+    const documentV1 = SwaggerModule.createDocument(app, configV1 ,);
+    SwaggerModule.setup(documentVersion1, app, documentV1, SwaggerConfig.createCustomOptions());
+    return final
+  }
+}
