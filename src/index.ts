f8014357ac1bf364c85aed83892b0ce4f5888230
elyasbr <78977021+elyasbr@users.noreply.github.com>
2025-03-22 17:30:01 +0330

    1.0.10

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from './decorators';
 export * from './modules';
-export * from './interceptors';
+export * from './interceptors/response.interceptor';
 export * from './dtos';
 export * from './base';
