c208a1aa8b2311630637c31d0540869c1924afaf
elyasbr <78977021+elyasbr@users.noreply.github.com>
2025-03-26 19:22:09 +0330

    1.0.31

--- src/decorators/api-403.decorator.ts
+++ src/decorators/api-403.decorator.ts
@@ -1,35 +1,33 @@
-import { HttpStatus, applyDecorators } from '@nestjs/common';
+import { applyDecorators } from '@nestjs/common';
 import {
-    ApiBadRequestResponse,
     ApiExtraModels,
     ApiForbiddenResponse,
-    ApiOkResponse,
     getSchemaPath,
 } from '@nestjs/swagger';
 import { Pattern4xErrorDto } from '../dtos/pattern-4x-error.dto';
 
-export const Api403 = ( ) => {
+export const Api403 = () => {
     return applyDecorators(
-        ApiExtraModels( Pattern4xErrorDto ),
-        ApiForbiddenResponse({
-            description: 'Forbidden Response',
-            schema: {
-                type: 'object',
-                properties: {
-                    statusCode: {
-                        type: 'number',
-                        example: 403,
-                    },
-                    message: {
-                        type: 'object',
-                        $ref: getSchemaPath(Pattern4xErrorDto),
-                    } ,
-                    timestamp: {
-                        type: 'Date',
-                        example: new Date(),
-                    },
-                },
-            },
-        }),
+      ApiExtraModels(Pattern4xErrorDto),
+      ApiForbiddenResponse({
+          description: 'Forbidden Response',
+          schema: {
+              type: 'object',
+              properties: {
+                  statusCode: {
+                      type: 'number',
+                      example: 403,
+                  },
+                  message: {
+                      allOf: [{ $ref: getSchemaPath(Pattern4xErrorDto) }],
+                  },
+                  timestamp: {
+                      type: 'string',
+                      format: 'date-time',
+                      example: new Date().toISOString(),
+                  },
+              },
+          },
+      }),
     );
 };
