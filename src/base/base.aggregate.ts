1f96f8d9b1234c523f0771f2e058cc695a45d46e
elyasbr <78977021+elyasbr@users.noreply.github.com>
2025-03-27 21:04:07 +0330

    1.0.50

--- src/base/base.aggregate.ts
+++ src/base/base.aggregate.ts
@@ -1,10 +1,10 @@
-import { AggregateRoot, EventPublisher } from '@nestjs/cqrs';
+import { AggregateRoot } from '@nestjs/cqrs';
 import { v4 as uuidv4 } from 'uuid';
 
 export abstract class BaseAggregate extends AggregateRoot{
   private readonly _ID: string;
   private _state: any;
-   protected constructor(_publisher : EventPublisher) {
+   protected constructor() {
      super();
      this._ID = uuidv4();
   }
