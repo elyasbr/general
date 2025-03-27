36cb0913813efecd570e70be06d97d96a50da4bd
elyasbr <78977021+elyasbr@users.noreply.github.com>
2025-03-27 20:43:18 +0330

    1.0.49

--- src/base/base.aggregate.ts
+++ src/base/base.aggregate.ts
@@ -4,18 +4,13 @@ import { v4 as uuidv4 } from 'uuid';
 export abstract class BaseAggregate extends AggregateRoot{
   private readonly _ID: string;
   private _state: any;
-  private readonly publisher: EventPublisher
    protected constructor(_publisher : EventPublisher) {
      super();
      this._ID = uuidv4();
-     this.publisher = _publisher;
-
   }
   get id(): string {
     return this._ID;
   }
-  setContex() {
-    return this.publisher.mergeObjectContext(this )
-  }
+
 
 }
