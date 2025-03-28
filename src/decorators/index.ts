1085c3b4568b6cf2def1be4c86e7f59ecfd8497d
elyasbr <78977021+elyasbr@users.noreply.github.com>
2025-03-28 03:51:02 +0330

    1.0.60

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -5,7 +5,6 @@ export  * from  './api-201.decorator'
 export  * from  './api-201-pagination.decorator'
 export  * from  './api-400.decorator'
 export  * from  './api-403.decorator'
-export  * from  './api-4031.decorator'
 export  * from  './api-404.decorator'
 export  * from  './api-409.decorator'
 
